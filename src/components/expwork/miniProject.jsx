ccb84cd1065af791ba6ad355546a5268a9c21171
Nixsh <143414492+callmenixsh@users.noreply.github.com>
2025-03-16 19:07:38 +0530

    fix attempt merge error

--- src/components/expwork/miniProject.jsx
+++ src/components/expwork/miniProject.jsx
@@ -5,8 +5,7 @@ import NumberGuessThumb from "../../assets/content/number_guess.png";
 import TemperatureConvertorThumb from "../../assets/content/temperature_convertor.png";
 import DiceThumb from "../../assets/content/dice.png";
 import TicTacToeThumb from "../../assets/content/tictactoe.png";
-import ReminderThumb from "../../assets/content/reminder.png";
-
+import ReminderThumb from "../../assets/content/restreminder.png";
 import PassgenThumb from "../../assets/content/passwordgen.png";
 
 // Array to store project data
@@ -42,46 +41,49 @@ const projects = [
 		url: "https://callmenixsh.github.io/Hajime/Tic%20Tac%20Toe/index.html",
 	},
 	{
-		name: "Rest Reminder",
-		img: ReminderThumb,
-		url: "https://callmenixsh.github.io/Hajime/Reminder/index.html",
-	},
-	{
 		name: "Password Generator",
 		img: PassgenThumb,
 		url: "https://callmenixsh.github.io/Hajime/Password/index.html",
+	},
+	{
+		name: "Rest Reminder",
+		img: ReminderThumb,
+		url: "https://callmenixsh.github.io/Hajime/Reminder/index.html",
 	},
 ];
 
 const MiniProject = () => {
 	return (
-	  <div className="flex flex-col justify-center items-center my-28">
-		<h1 className="text-3xl underline pb-14 text-black dark:text-white ">Mini Projects</h1>
-  
-		{/* Responsive Grid with Wrapping */}
-		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6 mx-10">
-		  {projects.map((project, index) => (
-			<a
-			  key={index}
-			  className="group flex items-center justify-center flex-col p-1 border-2 border-black dark:border-white rounded-lg hover:scale-105 hover:rotate-2 transition-all overflow-hidden"
-			  href={project.url}
-			  target="_blank"
-			  rel="noopener noreferrer"
-			>
-			  <div className="w-44 h-28 overflow-hidden rounded-md">
-				<img
-				  className="w-full h-full object-cover"
-				  src={project.img}
-				  alt={project.name}
-				/>
-			  </div>
-			  <div className="group-hover:text-gradient text-black dark:text-white transition-all duration-300 text-sm mt-2 text-center">{project.name}</div>
-			</a>
-		  ))}
+		<div className="flex flex-col justify-center items-center my-28">
+			<h1 className="text-3xl underline pb-14 text-black dark:text-white ">
+				Mini Projects
+			</h1>
+
+			{/* Responsive Grid with Wrapping */}
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6 mx-10">
+				{projects.map((project, index) => (
+					<a
+						key={index}
+						className="group flex items-center justify-center flex-col p-1 border-2 border-black dark:border-white rounded-lg hover:scale-105 hover:rotate-2 transition-all overflow-hidden"
+						href={project.url}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<div className="w-44 h-28 overflow-hidden rounded-md">
+							<img
+								className="w-full h-full object-cover"
+								src={project.img}
+								alt={project.name}
+							/>
+						</div>
+						<div className="group-hover:text-gradient text-black dark:text-white transition-all duration-300 text-sm mt-2 text-center">
+							{project.name}
+						</div>
+					</a>
+				))}
+			</div>
 		</div>
-	  </div>
 	);
-  };
-  
-  
+};
+
 export default MiniProject;
